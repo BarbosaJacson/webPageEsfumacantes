429d663fce07017d439c314493b4546db7e73045
Jacson Barbosa <barbosajacson.jb@gmail.com>
2026-03-18 18:18:17 -0300

    feat: setup mysql connection and restructure project files

---
+++ esfumacante-api/src/test/java/br/com/esfumacante/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.esfumacante.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
